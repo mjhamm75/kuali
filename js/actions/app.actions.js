72df3875b874fa956d766c4ae8f1d2a279aa6261
Jason Hamm <jasonhamm.me@gmail.com>
2015-03-28 09:02:58 -0600

    Add new rest call to actions to get starship info

--- js/actions/app.actions.js
+++ js/actions/app.actions.js
@@ -1,5 +1,14 @@
-export default {
-	setStarship: function() {
+import request from 'superagent';
+import dispatcher from './../dispatchers/app.dispatcher.js';
+import constants from './../constants/app.constants.js';
 
+export default {
+	setStarship: function(url) {
+		request.get(url).end((err, res) => {
+			dispatcher.handleViewAction({
+				action: constants.SET_STARSHIP,
+				starship: res.body
+			});
+		})
 	}
 }
