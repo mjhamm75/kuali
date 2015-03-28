e6f3f263826e5d2f60893109d62ffc0e757c625d
Jason Hamm <jasonhamm.me@gmail.com>
2015-03-28 07:45:42 -0600

    Create first react component

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-module.exports = function() {
-	console.log("testing");
-}
+import React from 'react';
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<h1>Testing Stuff</h1>
+		)
+	}
+});
+
+React.render(<App />, document.body);
