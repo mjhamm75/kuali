fd7b9f5039a108a350bde4dc1d1777aab6f61ab2
Jason Hamm <jasonhamm.me@gmail.com>
2015-03-28 07:45:03 -0600

    Update js file being watched

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
+var config = require('./config.js');
 
 gulp.task('watch', function() {
-	gulp.watch(config.js.src, ['browserify']);
+	gulp.watch(config.js, ['browserify']);
 	gulp.watch(config.less.src, ['less']);
 });
