ceecb67dca4dcb946156b8fc452d2863766d61b6
Jason Hamm <jasonhamm.me@gmail.com>
2015-03-28 08:11:18 -0600

    Add formatting to ship list

--- js/components/starships.js
+++ js/components/starships.js
@@ -9,7 +9,18 @@ export default React.createClass({
 	render: function() {
 		var starships = this.state.starships;
 		var starshipsDOM = starships.map((ship, i) => {
-			return <li key={i}>{ship}</li>
+			return (
+				<li key={i}>
+					<dl>
+						<dt>Name</dt>
+						<dd>{ship.name}</dd>
+						<dt>Cost</dt>
+						<dd>{ship.cost_in_credits}</dd>
+						<dt>Pilots</dt>
+						<dd>{ship.pilots}</dd>
+					</dl>
+				</li>
+			)
 		});
 		return (
 			<ol>
